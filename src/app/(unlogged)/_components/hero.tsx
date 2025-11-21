937d54fb479d7ce8672c225ca70b3fcc5d4666de
Amauri Oliveira <amauriibate32@hotmail.com>
2025-11-21 18:55:08 -0300

    Criando grid clinicas

--- src/app/(unlogged)/_components/hero.tsx
+++ src/app/(unlogged)/_components/hero.tsx
@@ -9,9 +9,9 @@ import doctorImage from '../../../../public/doctor-hero.png';
 export function Hero() {
   return (
     <section className='bg-white'>
-      <div className='container mx-auto px-4 pt-20 sm:px-6 lg:px-8'>
+      <div className='container mx-auto px-4 pt-20 pb-4 sm:pb-0 sm:px-6 lg:px-8'>
         <main className='flex items-center justify-center'>
-          <article className=' flex-[2] space-y-8 max-w-3xl flex flex-col justify-center'>
+          <article className=' flex-2 space-y-8 max-w-3xl flex flex-col justify-center'>
             <h1 className='text-4xl lg:text-5xl font-bold max-w-2xl tracking-tight'>
               Encontre os melhores profissionais em um único lugar!
             </h1>
@@ -19,7 +19,9 @@ export function Hero() {
               Nós somos uma plataforma para profissionais da saúde com foco em agilizar seu atendimento de forma
               simplificada e organizada.
             </p>
-            <Button className='bg-emerald-500 hover:bg-emerald-600 text-white w-fit px-6 font-semibold'>Profissionais disponíveis</Button>
+            <Button className='bg-emerald-500 hover:bg-emerald-600 text-white w-fit px-6 font-semibold'>
+              Profissionais disponíveis
+            </Button>
           </article>
           <div className='hidden lg:block'>
             <Image
--- src/app/(unlogged)/page.tsx
+++ src/app/(unlogged)/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from './_components/header';
 import { Hero } from './_components/hero';
+import { Profissional } from './_components/professionals';
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <div>
         <Hero />
+        <Profissional />
       </div>
     </div>
   );
